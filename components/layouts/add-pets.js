0b860f781e22ef9e0a028623f4aaa0a7dfb685f8
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-11-18 20:32:52 +0700

    Add new pets
    text only

--- components/layouts/add-pets.js
+++ components/layouts/add-pets.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from "react"
 import Link from 'next/link'
+import Axios from "axios"
 
 //Font awesome icon
 import { library, text } from "@fortawesome/fontawesome-svg-core"
@@ -13,7 +14,13 @@ function AddPets() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [nav, setToogleNav] = useState(false);
-    const [gender, setGender] = useState("male");
+    const [petsGender, setGender] = useState("male");
+    const [favorite, setFavorite] = useState(false);
+    const [petsName, setPetsName] = useState("");
+    const [petsType, setPetsType] = useState("");
+    const [petsStatus, setPetsStatus] = useState("");
+    const [petsBorn, setPetsBorn] = useState("");
+    const [petsDesc, setPetsDesc] = useState("");
     const [items, setItems] = useState([]);
   
     useEffect(() => {
@@ -31,11 +38,39 @@ function AddPets() {
       )
     },[])
 
+    //Insert new pets
+    async function addPets (e) {
+      // e.preventDefault();
+      const pets_name = petsName
+      const pets_type = petsDesc
+      const pets_gender = petsGender
+      const pets_desc = petsDesc
+      const pets_status = petsStatus
+      const pets_born = petsBorn
+      const pets_url_image = null
+      const pets_url_video = null
+
+      try {
+        await Axios.post("http://localhost:9000/addPets/1", {
+          pets_name,
+          pets_type,
+          pets_gender,
+          pets_desc,
+          pets_status,
+          pets_born,
+          pets_url_image,
+          pets_url_video
+        });
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+
     //Converter
     const data = Object.values(items);
 
     function getButtonGender(){
-      if(gender == "male"){
+      if(petsGender == "male"){
         return (
           <button className='btn-form-header-toogle male' title='Set gender Male' onClick={(e) => setGender("female")}><FontAwesomeIcon icon={faMars} width="20px"/></button>
         );
@@ -44,6 +79,27 @@ function AddPets() {
           <button className='btn-form-header-toogle female' title='Set gender Female' onClick={(e) => setGender("male")}><FontAwesomeIcon icon={faVenus} width="20px"/></button>
         );
       }
+    }
+
+    function getButtonFavorite(){
+      if(favorite == false){
+        return (
+          <button className='btn-form-header-toogle favorite' title='Add to favorite pets' onClick={(e) => setFavorite(true)}><FontAwesomeIcon icon={faHeart} width="20px"/></button>
+        );
+      } else {
+        return (
+          <button className='btn-form-header-toogle normal' title='Remove from favorite pets' onClick={(e) => setFavorite(false)}><FontAwesomeIcon icon={faHeart} width="20px"/></button>
+        );
+      }
+    }
+
+    function getSubmitButton(){
+      if((petsName.length != 0)&&(typeof document !== "undefined")){
+        return (
+          <button className='btn-form-header-toogle save float-end' title='Save new pets' style={{'marginRight': "15px" }} onClick={(e)=> addPets()}
+            ><FontAwesomeIcon icon={faSave} width="20px"/></button>
+        );
+      } 
     }
 
     var loadFile = function(event) {
@@ -66,17 +122,17 @@ function AddPets() {
               <div className='modal-form-body'>
                 <div className='row modal-body-header-set'>
                   <label htmlFor="file" className="btn-form-header-toogle upload p-3" title='Upload Image'> <FontAwesomeIcon icon={faImage} width="20px"/> </label>
-                  <button className='btn-form-header-toogle favorite' title='Add to favorite pets'><FontAwesomeIcon icon={faHeart} width="20px"/></button>
+                  {getButtonFavorite()}
                   {getButtonGender()}
-                  <button className='btn-form-header-toogle save float-end' title='Save new pets' style={{'marginRight': "15px" }}><FontAwesomeIcon icon={faSave} width="20px"/></button>
+                  {getSubmitButton()}
                 </div>
                 <form className='p-3'>
                   <div className='row'>
                     <div className='col-lg-6 col-md-6 col-sm-12'>
                       <label className='mt-4'>Pets Name</label>
-                      <input className='form-control mt-1' type={'text'}></input>
+                      <input className='form-control mt-1' type={'text'} onChange={(e)=> setPetsName(e.target.value)} onBlur={(e)=> getSubmitButton()}></input>
                       <label className='mt-2'>Type</label>
-                      <select className="form-select mt-1">
+                      <select className="form-select mt-1" onChange={(e)=> setPetsType(e.target.value)}>
                         {
                           data.map((val, i, index) => {
                             if(i == 0){
@@ -88,17 +144,17 @@ function AddPets() {
                         }
                       </select>
                       <label className='mt-2'>Status</label>
-                      <select className="form-select mt-1">
+                      <select className="form-select mt-1" onChange={(e)=> setPetsStatus(e.target.value)}>
                         <option selected defaultValue="healthy">Healthy</option>
                         <option defaultValue="sick">Sick</option>
                         <option defaultValue="pregnant">Pregnant</option>
                       </select>
                       <label className='mt-2'>Date Birth</label>
-                      <input className='form-control mt-1' type={'date'}></input>
+                      <input className='form-control mt-1' type={'date'} onChange={(e)=> setPetsBorn(e.target.value)}></input>
                     </div>
                     <div className='col-lg-6 col-md-6 col-sm-12'>
                       <label className='mt-4'>Description <b className='label-info'>(Optional)</b></label>
-                      <textarea className='form-control mt-1' type={'text'} rows="4"></textarea>
+                      <textarea className='form-control mt-1' type={'text'} rows="4" onChange={(e)=> setPetsDesc(e.target.value)}></textarea>
                     </div>
                   </div>
                 </form>
--- pages/api/router/pets.js
+++ pages/api/router/pets.js
@@ -29,4 +29,30 @@ router.get('/getAllPetsType', (req, res) => {
     })
 })
 
+//Insert pets
+router.post('/addPets/:id', (req, res) => {
+    const id = req.params.id;
+    const pets_name = req.body.pets_name
+    const pets_type = req.body.pets_type
+    const pets_gender = req.body.pets_gender
+    const pets_desc = req.body.pets_desc
+    const pets_status = req.body.pets_status
+    const pets_born = req.body.pets_born
+    const pets_url_image = req.body.pets_url_image
+    const pets_url_video = req.body.pets_url_video
+    const created_at = new Date()
+    const updated_at = new Date()
+
+    connection.query("INSERT INTO " +
+        "pets (id, user_id, pets_name, pets_type, pets_gender, pets_desc, pets_status, pets_born, pets_url_image, pets_url_video, created_at, updated_at) " +
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+        [null, id, pets_name, pets_type, pets_gender, pets_desc, pets_status, pets_born, pets_url_image, pets_url_video, created_at, updated_at], (error, rows, fields) => {
+        if (error) {
+            res.status(400).json({ msg: "Error :" + error })
+        } else {
+            res.status(200).json({ msg: "Insert Pets Success",status:200, data: rows })
+        }
+    })
+})
+
 module.exports = router
